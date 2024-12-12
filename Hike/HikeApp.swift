c5000fbebaf61df8584309546c9b6f496a6b7153
Siyanbola Akin <akin.siyanbola@gmail.com>
2024-12-12 12:11:37 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by DEV-AKIN on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
