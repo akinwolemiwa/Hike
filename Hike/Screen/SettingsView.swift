4e011d65f517472219e20ed8a2371ff4bf47ef25
Siyanbola Akin <akin.siyanbola@gmail.com>
2025-01-08 11:45:14 +0100

    SwiftUI Sheet

---
+++ Hike/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Hike
+//
+//  Created by DEV-AKIN on 1/7/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
